23fd55702197dc8e0d36d1dd276ed6cc06675850
tb_seol <tb.seol@ms.saigeresearch.ai>
2020-08-12 18:49:48 +0900

    기존의 Button에서 적용했던 Style을 모두 Styles/Button.xaml로 옮긴 뒤 App.xaml에서 일괄적으로 적용합니다.

---
+++ TimeCalculator/TimeCalculator/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace TimeCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
