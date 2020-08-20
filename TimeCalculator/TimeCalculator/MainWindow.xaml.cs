86f074111d296f5241692520a5a41c12d1af362f
tb_seol <tb.seol@ms.saigeresearch.ai>
2020-08-20 18:26:03 +0900

    SubViewer의 이름을 MiniViewer로 변경하고, ItemsControl 컨트롤로 변경합니다

--- TimeCalculator/TimeCalculator/MainWindow.xaml.cs
+++ TimeCalculator/TimeCalculator/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -11,158 +11,134 @@ namespace TimeCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static string SUB_VIEWER_TITLE = "대략 같음";
-        private static string[] SUB_VIEWER_VALUES = new string[] { "yr", "주", "d", "시간", "분", "s", "ms", "us" };
+        private static string MINI_VIEWER_TITLE = "대략 같음";
+        private static string[] MINI_VIEWER_VALUES = new string[] { "yr", "주", "d", "시간", "분", "s", "ms", "us" };
 
         private Time mTime;
+        private ViewerGrid mMainViewer;
+        private ViewerGrid mSubViewer;
         private ETimeUnit mTopViewerDefaultUnit = ETimeUnit.Minute;
         private ETimeUnit mBottomViewerDefaultUnit = ETimeUnit.Hour;
 
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
 
-            mTime = new Time(mTopViewerDefaultUnit);
             xViewerGrid_Top.SetTimeUnit(mTopViewerDefaultUnit);
             xViewerGrid_Bottom.SetTimeUnit(mBottomViewerDefaultUnit);
 
-            xViewerGrid_Top.xComboBox_TimeUnit.SelectionChanged += XComboBox_TimeUnit_SelectionChanged_Top;
-            xViewerGrid_Bottom.xComboBox_TimeUnit.SelectionChanged += XComboBox_TimeUnit_SelectionChanged_Bottom;
-            DataContext = this;
+            mMainViewer = xViewerGrid_Top;
+            mSubViewer = xViewerGrid_Bottom;
+
+            mTime = new Time(mMainViewer.GetTimeUnit());
+
+            xViewerGrid_Top.xTextBlock_Time.MouseDown += XTextBlock_Time_MouseDown;
+            xViewerGrid_Bottom.xTextBlock_Time.MouseDown += XTextBlock_Time_MouseDown;
+
+            xViewerGrid_Top.xComboBox_TimeUnit.SelectionChanged += XComboBox_TimeUnit_SelectionChanged;
+            xViewerGrid_Bottom.xComboBox_TimeUnit.SelectionChanged += XComboBox_TimeUnit_SelectionChanged;
         }
 
         protected override void OnClosed(EventArgs e)
         {
             base.OnClosed(e);
-            xViewerGrid_Top.xComboBox_TimeUnit.SelectionChanged -= XComboBox_TimeUnit_SelectionChanged_Top;
-            xViewerGrid_Bottom.xComboBox_TimeUnit.SelectionChanged -= XComboBox_TimeUnit_SelectionChanged_Bottom;
+
+            xViewerGrid_Top.xTextBlock_Time.MouseDown -= XTextBlock_Time_MouseDown;
+            xViewerGrid_Bottom.xTextBlock_Time.MouseDown -= XTextBlock_Time_MouseDown;
+
+            xViewerGrid_Top.xComboBox_TimeUnit.SelectionChanged -= XComboBox_TimeUnit_SelectionChanged;
+            xViewerGrid_Bottom.xComboBox_TimeUnit.SelectionChanged -= XComboBox_TimeUnit_SelectionChanged;
         }
 
-        private void xButton_NumPad_Clear_Click(object sender, RoutedEventArgs e)
+        private void xButton_NumPad_Click_Clear(object sender, RoutedEventArgs e)
         {
             mTime.Clear();
             updateViewer();
         }
 
-        private void xButton_NumPad_Delete_Click(object sender, RoutedEventArgs e)
+        private void xButton_NumPad_Click_Delete(object sender, RoutedEventArgs e)
         {
             mTime.RemoveLastCharacter();
             updateViewer();
         }
 
-        private void xButton_NumPad_Seven_Click(object sender, RoutedEventArgs e)
+        private void xButton_NumPad_Click_Normal(object sender, RoutedEventArgs e)
         {
-            mTime.AppendCharacter('7');
+            Button button = sender as Button;
+            char character = char.Parse(button.Content.ToString());
+
+            Debug.Assert(character >= '0' && character <= '9' || character == '.');
+
+            mTime.AppendCharacter(character);
             updateViewer();
         }
 
-        private void xButton_NumPad_Eight_Click(object sender, RoutedEventArgs e)
+        private void XTextBlock_Time_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            mTime.AppendCharacter('8');
-            updateViewer();
+            if (sender == mMainViewer.xTextBlock_Time)
+            {
+                return;
+            }
+
+            ViewerGrid temp = mMainViewer;
+            mMainViewer = mSubViewer;
+            mSubViewer = temp;
+
+            mTime.UnitForConvert = mMainViewer.GetTimeUnit();
+            mTime.Clear();
         }
 
-        private void xButton_NumPad_Nine_Click(object sender, RoutedEventArgs e)
+        private void XComboBox_TimeUnit_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            mTime.AppendCharacter('9');
-            updateViewer();
-        }
-
-        private void xButton_NumPad_Four_Click(object sender, RoutedEventArgs e)
-        {
-            mTime.AppendCharacter('4');
-            updateViewer();
-        }
-
-        private void xButton_NumPad_Five_Click(object sender, RoutedEventArgs e)
-        {
-            mTime.AppendCharacter('5');
-            updateViewer();
-        }
-
-        private void xButton_NumPad_Six_Click(object sender, RoutedEventArgs e)
-        {
-            mTime.AppendCharacter('6');
-            updateViewer();
-        }
-
-        private void xButton_NumPad_One_Click(object sender, RoutedEventArgs e)
-        {
-            mTime.AppendCharacter('1');
-            updateViewer();
-        }
-
-        private void xButton_NumPad_Two_Click(object sender, RoutedEventArgs e)
-        {
-            mTime.AppendCharacter('2');
-            updateViewer();
-        }
-
-        private void xButton_NumPad_Three_Click(object sender, RoutedEventArgs e)
-        {
-            mTime.AppendCharacter('3');
-            updateViewer();
-        }
-
-        private void xButton_NumPad_Zero_Click(object sender, RoutedEventArgs e)
-        {
-            mTime.AppendCharacter('0');
-            updateViewer();
-        }
-
-        private void xButton_NumPad_Dot_Click(object sender, RoutedEventArgs e)
-        {
-            mTime.AppendCharacter('.');
-            updateViewer();
-        }
-
-        private void XComboBox_TimeUnit_SelectionChanged_Top(object sender, SelectionChangedEventArgs e)
-        {
-            mTime.UnitForConvert = xViewerGrid_Top.GetTimeUnit();
-            updateViewer();
-        }
-
-        private void XComboBox_TimeUnit_SelectionChanged_Bottom(object sender, SelectionChangedEventArgs e)
-        {
+            mTime.UnitForConvert = mMainViewer.GetTimeUnit();
             updateViewer();
         }
 
         private void updateViewer()
         {
-            const double MIN_VALUE = 0.01;
-
             List<double> times = mTime.GetAllTimes();
-
-            StringBuilder sb = new StringBuilder(128);
-            for (int i = 0; i < times.Count; i++)
-            {
-                double value = times[times.Count - 1 - i];
-
-                if (value >= MIN_VALUE)
-                {
-                    sb.Append(formatTimeValue(value))
-                        .Append(" ")
-                        .Append(SUB_VIEWER_VALUES[i])
-                        .Append("   ");
-                }
-            }
+            string miniViewerTitle = (times[0] > 0) ? MINI_VIEWER_TITLE : "";
 
             xViewerGrid_Top.UpdateTime(times);
             xViewerGrid_Bottom.UpdateTime(times);
 
-            string title = (sb.Length > 0) ? SUB_VIEWER_TITLE : "";
-            xTextBlock_SubViewerTitle.Text = title;
-            xTextBlock_SubViewerBody.Text = sb.ToString();
+            xTextBlock_MiniViewerTitle.Text = miniViewerTitle;
+            xItemsControl_MiniViewerValues.ItemsSource = formatMiniViewerTimes(times);
         }
 
-        private string formatTimeValue(double value)
+        private string formatMiniViewerTime(double time)
         {
-            if (value < 10)
+            if (time < 10)
             {
-                return Math.Round(value, 2).ToString();
+                return Math.Round(time, 2).ToString();
             }
 
-            return Math.Round(value).ToString("N0");
+            return Math.Round(time).ToString("N0");
+        }
+
+        private List<string> formatMiniViewerTimes(List<double> times)
+        {
+            const double MIN_VALUE = 0.01;
+
+            List<string> formattedTimes = new List<string>(16);
+
+            for (int i = 0; i < times.Count; i++)
+            {
+                if (i == (int)mMainViewer.GetTimeUnit() || i == (int)mSubViewer.GetTimeUnit())
+                {
+                    continue;
+                }
+
+                double time = times[times.Count - 1 - i];
+
+                if (time >= MIN_VALUE)
+                {
+                    formattedTimes.Add(formatMiniViewerTime(time) + MINI_VIEWER_VALUES[i] + "  ");
+                }
+            }
+
+            return formattedTimes;
         }
     }
 }
