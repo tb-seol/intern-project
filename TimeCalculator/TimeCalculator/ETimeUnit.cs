25f49ae1c4dbed80f9525222f5fab804af52b79a
tb_seol <tb.seol@ms.saigeresearch.ai>
2020-08-24 10:40:57 +0900

    ComboBoxItem Text를 Enum의 Description으로 설정합니다

--- TimeCalculator/TimeCalculator/ETimeUnit.cs
+++ TimeCalculator/TimeCalculator/ETimeUnit.cs
@@ -1,5 +1,7 @@
 ﻿namespace TimeCalculator
 {
+    using System.ComponentModel;
+
     /// <summary>
     /// Types of time units.
     /// </summary>
@@ -8,41 +10,49 @@
         /// <summary>
         /// Represents a 마이크로초.
         /// </summary>
+        [Description("마이크로초")]
         MicroSecond,
 
         /// <summary>
         /// Represents a 밀리초.
         /// </summary>
+        [Description("밀리초")]
         MilliSecond,
 
         /// <summary>
         /// Represents a 초.
         /// </summary>
+        [Description("초")]
         Second,
 
         /// <summary>
         /// Represents a 분.
         /// </summary>
+        [Description("분")]
         Minute,
 
         /// <summary>
         /// Represents a 시간.
         /// </summary>
+        [Description("시간")]
         Hour,
 
         /// <summary>
         /// Represents a 일.
         /// </summary>
+        [Description("일")]
         Day,
 
         /// <summary>
         /// Represents a 주.
         /// </summary>
+        [Description("주")]
         Week,
 
         /// <summary>
         /// Represents a 년.
         /// </summary>
+        [Description("년")]
         Year
     }
 }
---
+++ TimeCalculator/TimeCalculator/EnumDescriptionConverter.cs
@@ -0,0 +1,38 @@
+﻿namespace TimeCalculator
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.Reflection;
+    using System.Windows.Data;
+
+    public class EnumDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return GetEnumDescription((Enum)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string GetEnumDescription(Enum enumObj)
+        {
+            FieldInfo fi = enumObj.GetType().GetField(enumObj.ToString());
+
+            object[] attribArray = fi.GetCustomAttributes(false);
+
+            if (attribArray.Length == 0)
+            {
+                return enumObj.ToString();
+            }
+            else
+            {
+                var attrib = attribArray[0] as DescriptionAttribute;
+                return attrib.Description;
+            }
+        }
+    }
+}
