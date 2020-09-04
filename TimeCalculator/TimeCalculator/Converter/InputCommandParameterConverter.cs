252bd946d8da2666f6c125a4d0ce137ab647a00e
tb_seol <tb.seol@ms.saigeresearch.ai>
2020-09-04 17:59:23 +0900

    InputCommand의 파라미터를 알맞게 변경해주는 컨버터를 추가합니다

---
+++ TimeCalculator/TimeCalculator/Converter/InputCommandParameterConverter.cs
@@ -0,0 +1,65 @@
+﻿namespace TimeCalculator.Converter
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+    using System.Windows.Input;
+
+    public class InputCommandParameterConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value switch
+            {
+                Key key => GetInputCommandParameterFromKeyBinding(key),
+                string content => GetInputCommandParameterFromButtonContent(content),
+                _ => throw new InvalidOperationException()
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static char GetInputCommandParameterFromKeyBinding(Key input)
+        {
+            return input switch
+            {
+                Key.NumPad0 => '0',
+                Key.NumPad1 => '1',
+                Key.NumPad2 => '2',
+                Key.NumPad3 => '3',
+                Key.NumPad4 => '4',
+                Key.NumPad5 => '5',
+                Key.NumPad6 => '6',
+                Key.NumPad7 => '7',
+                Key.NumPad8 => '8',
+                Key.NumPad9 => '9',
+                Key.Decimal => '.',
+                _ => throw new NotSupportedException(input.ToString()),
+            };
+        }
+
+        private static char GetInputCommandParameterFromButtonContent(string input)
+        {
+            switch (input)
+            {
+                case "0":
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case ".":
+                    return char.Parse(input.ToString());
+                default:
+                    throw new NotSupportedException(input.ToString());
+            }
+        }
+    }
+}
